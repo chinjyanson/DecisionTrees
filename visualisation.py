ec0f5acaa40f8aa537c10414c676951fb871298b
chinjyanson <anson.chin22@imperial.ac.uk>
2024-10-24 14:58:55 +0100

    <refactor> trying to integrate the visualisation with the main code

--- visualisation.py
+++ visualisation.py
@@ -11,13 +11,14 @@ def assign_positions(tree, x=0, y=0, vertical_dist=1, horizontal_dist=2, positio
         positions = {}
 
     # Assign current node position
-    positions[tree['value']] = (x, y, tree['attribute'])
+    print(tree.attribute)
+    positions[tree.val] = (x, y, tree.attribute)
 
     # Calculate positions for the left and right children
-    if tree.get('left'):
-        positions = assign_positions(tree['left'], x - horizontal_dist, y - vertical_dist, vertical_dist, horizontal_dist / 1.5, positions)
-    if tree.get('right'):
-        positions = assign_positions(tree['right'], x + horizontal_dist, y - vertical_dist, vertical_dist, horizontal_dist / 1.5, positions)
+    if tree.left:
+        positions = assign_positions(tree.left, x - horizontal_dist, y - vertical_dist, vertical_dist, horizontal_dist / 1.5, positions)
+    if tree.right:
+        positions = assign_positions(tree.right, x + horizontal_dist, y - vertical_dist, vertical_dist, horizontal_dist / 1.5, positions)
 
     return positions
 
@@ -27,45 +28,45 @@ def plot_tree(tree, positions):
 
     # Plot the edges
     def plot_edges(tree, positions):
-        if tree.get('left'):
-            x_values = [positions[tree['value']][0], positions[tree['left']['value']][0]]
-            y_values = [positions[tree['value']][1], positions[tree['left']['value']][1]]
+        if tree.left:
+            x_values = [positions[tree.val][0], positions[tree.left.val][0]]
+            y_values = [positions[tree.val][1], positions[tree.left.val][1]]
             ax.plot(x_values, y_values, 'k-', lw=1)  # black line for edges
-            plot_edges(tree['left'], positions)
-        if tree.get('right'):
-            x_values = [positions[tree['value']][0], positions[tree['right']['value']][0]]
-            y_values = [positions[tree['value']][1], positions[tree['right']['value']][1]]
+            plot_edges(tree.left, positions)
+        if tree.right:
+            x_values = [positions[tree.val][0], positions[tree.right.val][0]]
+            y_values = [positions[tree.val][1], positions[tree.right.val][1]]
             ax.plot(x_values, y_values, 'k-', lw=1)
-            plot_edges(tree['right'], positions)
+            plot_edges(tree.right, positions)
 
     plot_edges(tree, positions)
 
     # Plot the nodes
-    for node, (x, y, attribute) in positions.items():
-        ax.text(x, y, str(f"{attribute} < {node}"), fontsize=12, ha='center', va='center', bbox=dict(boxstyle='round,pad=0.3', facecolor='lightblue', edgecolor='black'))
+    for node_val, (x, y, attribute) in positions.items():
+        ax.text(x, y, f"X{attribute} < {node_val}", fontsize=12, ha='center', va='center', bbox=dict(boxstyle='round,pad=0.3', facecolor='lightblue', edgecolor='black'))
 
     ax.set_aspect('equal')
     ax.set_axis_off()
     plt.savefig("tree.png")
 
-binary_tree = {
-    'value': 1,
-    'attribute': 'X1',
-    'left': {
-        'value': 2,
-        'attribute': 'X2',
-        'left': {'value': 4, 'attribute': 'X5'},
-        'right': {'value': 5, 'attribute': 'X3'}
-    },
-    'right': {
-        'value': 3,
-        'attribute': 'X4',
-        'right': {'value': 6, 'attribute': 'X6'}
-    }
-}
+# binary_tree = {
+#     'value': 1,
+#     'attribute': 'X1',
+#     'left': {
+#         'value': 2,
+#         'attribute': 'X2',
+#         'left': {'value': 4, 'attribute': 'X5'},
+#         'right': {'value': 5, 'attribute': 'X3'}
+#     },
+#     'right': {
+#         'value': 3,
+#         'attribute': 'X4',
+#         'right': {'value': 6, 'attribute': 'X6'}
+#     }
+# }
 
-# Assign positions to the tree nodes
-positions = assign_positions(binary_tree)
+# # Assign positions to the tree nodes
+# positions = assign_positions(binary_tree)
 
-# Plot the tree
-plot_tree(binary_tree, positions)
+# # Plot the tree
+# plot_tree(binary_tree, positions)
